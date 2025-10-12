927ac1b3da47a84b138462243c812b4ff6f6bace
NaVteJSingH18 <navtej.dgc@gmail.com>
2025-10-12 08:26:50 +0530

    new content

---
+++ first-app/src/App.jsx
@@ -0,0 +1,21 @@
+import Card from "./components/Card";
+import Footer from "./components/footer";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="cards">
+        <Card title="card1" description ="card 1 desc"/>
+        <Card title="card2" description ="card 2 desc"/>
+        <Card title="card3" description ="card 3 desc"/>
+        <Card title="card4" description ="card 4 desc"/>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ first-app/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './footer.css'
+function footer() {
+  return (
+    <div className='footer'>
+      Copyright @2025 | All rights reserved
+    </div>
+  )
+}
+
+export default footer
